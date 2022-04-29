83508a2669a798ebe5cee66142c9579ff7e06165
subashkhatri36 <subashkhatri36@gmail.com>
2022-04-29 08:07:46 +0545

    sperate method

--- lib/walk_through/walk_through_page.dart
+++ lib/walk_through/walk_through_page.dart
@@ -73,119 +73,174 @@ class _PagesState extends State<_Pages> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          flex: 7,
-          child: FadeInImage.assetNetwork(
-            placeholder: 'assets/images/test.png',
-            image: widget.walkThroughModel.image ?? '',
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
+        _StudentImageWidget(
+          image: widget.walkThroughModel.image ?? '',
         ),
-        Expanded(
-          flex: 3,
-          child: Stack(
-            children: [
-              Container(
-                alignment: Alignment.centerRight,
-                color: walkThroughEnumToColor(
-                    widget.walkThroughModel.countryColor ?? 'blue'),
-                padding: const EdgeInsets.all(8.0),
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.06,
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 6,
-                      child: Center(
-                        child: Text(
-                          widget.walkThroughModel.country?.toUpperCase() ?? '',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6
-                              ?.copyWith(color: Colors.white),
+        _CountryDescriptionWidget(
+          titleColor: walkThroughEnumToColor(
+              widget.walkThroughModel.countryColor ?? 'blue'),
+          totalPage: widget.totalPage,
+          index: widget.index,
+          description: widget.walkThroughModel.description ?? '',
+          mapImage: widget.walkThroughModel.mapImage ?? '',
+          onPageIndexChange: () => changePage(widget.index),
+          title: widget.walkThroughModel.country ?? '',
+          onPressed: widget.onPressed,
+        )
+      ],
+    );
+  }
+}
+
+class _StudentImageWidget extends StatelessWidget {
+  const _StudentImageWidget({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 7,
+      child: FadeInImage.assetNetwork(
+        placeholder: 'assets/images/test.png',
+        image: image,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class _CountryDescriptionWidget extends StatelessWidget {
+  const _CountryDescriptionWidget({
+    Key? key,
+    required this.titleColor,
+    required this.title,
+    required this.description,
+    required this.mapImage,
+    this.onPressed,
+    required this.totalPage,
+    required this.index,
+    required this.onPageIndexChange,
+  }) : super(key: key);
+  final Color titleColor;
+  final String title;
+  final String description;
+  final String mapImage;
+  final int index;
+  final int totalPage;
+  final VoidCallback? onPressed;
+  final VoidCallback? onPageIndexChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: Stack(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            color: titleColor,
+            padding: const EdgeInsets.all(8.0),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.06,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: Center(
+                    child: Text(
+                      title,
+
+                      // widget.walkThroughModel.country?.toUpperCase() ?? '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          ?.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.topRight,
+            padding: const EdgeInsets.only(
+              right: 5,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.06,
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 5, top: 10),
+                          child: Text(
+                            description,
+                            // widget.walkThroughModel.description ?? '',
+                            textAlign: TextAlign.justify,
+                            maxLines: 10,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                ?.copyWith(fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Container(),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                alignment: Alignment.topRight,
-                padding: const EdgeInsets.only(
-                  right: 5,
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
+                    FadeInImage.assetNetwork(
+                      placeholder: 'assets/images/test.png',
+                      image: mapImage,
+                      // widget.walkThroughModel.mapImage ?? '',
+                      width: MediaQuery.of(context).size.width * 0.4,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.4,
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.06,
+                          //TODO: change button Color
+                          ElevatedButton(
+                            onPressed: onPressed,
+                            child: const Text('Skip'),
                           ),
-                          Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 5, top: 10),
-                              child: Text(
-                                widget.walkThroughModel.description ?? '',
-                                textAlign: TextAlign.justify,
-                                maxLines: 10,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText1
-                                    ?.copyWith(fontWeight: FontWeight.bold),
-                                overflow: TextOverflow.ellipsis,
-                              ),
+                          if (index < totalPage - 1)
+                            ElevatedButton(
+                              onPressed: () => onPageIndexChange,
+                              child: const Text('Next'),
                             ),
-                          ),
                         ],
                       ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FadeInImage.assetNetwork(
-                          placeholder: 'assets/images/test.png',
-                          image: widget.walkThroughModel.mapImage ?? '',
-                          width: MediaQuery.of(context).size.width * 0.4,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(
-                            left: 10,
-                          ),
-                          width: MediaQuery.of(context).size.width * 0.4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              //TODO: change button Color
-                              ElevatedButton(
-                                onPressed: widget.onPressed,
-                                child: const Text('Skip'),
-                              ),
-                              if (widget.index < widget.totalPage - 1)
-                                ElevatedButton(
-                                  onPressed: () => changePage(widget.index + 1),
-                                  child: const Text('Next'),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
